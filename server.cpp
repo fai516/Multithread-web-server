65b8ef867781c28fff2ea94fbd69c9f8329bbbcb
Fai <fai@r06guozh.managed.mst.edu>
2017-10-20 12:23:33 -0500

    update

--- server.cpp
+++ server.cpp
@@ -1,17 +1,4 @@
-/************************************************************************/
-/*   PROMGRAMMER NAMES: Jerry Linder and the Google search engine		*/	
-/*   PROGRAM NAME: server.cpp  (works with client.cpp)                  */
-/*	 ASSIGNMENT: C3800 Operating Systems Project 3 (Inst: Fikret Ercal) */
-/*                                                                      */
-/*   Server creates a socket to listen for the connection from Client   */
-/*   Client asks for a display name which then gets read by the Server. */
-/*   When the communication established, Server echoes data from Client */
-/*   and writes them back.                                              */
-/*                                                                      */
-/*   COMPILE:         g++ server.cpp -pthread -o server    		        */
-/*	 TO RUN:		  ./server											*/
-/*                                                                      */
-/************************************************************************/
+
 
 #include <sys/socket.h>  /* define socket */
 #include <netinet/in.h>  /* define internet socket */
